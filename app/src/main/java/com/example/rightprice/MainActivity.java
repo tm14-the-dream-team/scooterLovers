e3497b7567fb682dfb932b5937e2d78a499ee19a
jakeypakey <Mic_CS1A>
2019-05-13 19:06:56 -0700

    login button takes you to map now, login page needs refomatting but basic functionality to launch map is here

--- app/src/main/java/com/example/rightprice/MainActivity.java
+++ app/src/main/java/com/example/rightprice/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.rightprice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void launchMap(View view) {
+        System.out.println("IT WORKED");
+        Intent launchMap = new Intent(this, Map.class);
+        startActivity(launchMap);
     }
 }
