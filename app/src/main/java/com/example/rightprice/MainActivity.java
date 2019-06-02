db7534e3cfe9985a55722e4805687ae4b8c45445
Ryan <rwmcclur@ucsd.edu>
2019-06-01 21:25:16 -0700

    no more launch man func

--- app/src/main/java/com/example/rightprice/MainActivity.java
+++ app/src/main/java/com/example/rightprice/MainActivity.java
@@ -88,12 +88,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    protected void launchMap(View view) {
-        System.out.println("LAUNCH MAP");
-        Intent launchMap = new Intent(this, Map.class);
-        startActivity(launchMap);
-        finish();
-    }
+
 
     protected void login(View view) {
         mAuth = FirebaseAuth.getInstance();
