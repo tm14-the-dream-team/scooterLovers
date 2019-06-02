330d4b2ac2ce57206d87047d1c47c6129217bd51
Isa <anoui@ucsd.edu>
2019-06-01 20:26:14 -0700

    current location

--- app/src/main/java/com/example/rightprice/Map.java
+++ app/src/main/java/com/example/rightprice/Map.java
@@ -1,18 +1,35 @@
 package com.example.rightprice;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,9 +40,17 @@ import java.util.HashMap;
 
 import static com.google.firebase.auth.FirebaseAuth.getInstance;
 
-public class Map extends FragmentActivity implements OnMapReadyCallback {
+public class Map extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+    private Location lastLocation;
+    private Marker currentUserLocationMarker;
+    private static final int Request_User_Location_Code = 99;
+
     private ImageButton settingsButton;
     private ImageButton filterButton;
     private Button birdButton;
@@ -48,6 +73,9 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
         mAuth = getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkUserLocationPermission();
+        }
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -69,7 +97,7 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
         logoutButton = (Button) findViewById(R.id.logout_button);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 // implement logging out.
                 FirebaseAuth.getInstance().signOut();
 
@@ -83,8 +111,8 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
         // Shows settings when pressing the Settings Button
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                if (servicesLayer.getVisibility()==View.VISIBLE) {
+            public void onClick(View v) {
+                if (servicesLayer.getVisibility() == View.VISIBLE) {
                     servicesLayer.setVisibility(View.INVISIBLE);
                 } else {
                     servicesLayer.setVisibility(View.VISIBLE);
@@ -95,7 +123,7 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
         //add or delete bird
         birdButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 //handle bird login
                 FirebaseUser user = mAuth.getCurrentUser();
                 String userUID = user.getUid();
@@ -110,14 +138,14 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
         //add or delete lime
         limeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 //handle lime login
             }
         });
         //add or delete spin
         spinButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 //handle spin login
             }
         });
@@ -174,14 +202,14 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
         filterOptionsLayer = (LinearLayout) findViewById(R.id.filter_options_layer);
         filterOptionsLayer.setVisibility(View.INVISIBLE);
         // Shows filter menu when pressing the filter Button
-        filterButton.setOnClickListener(new View.OnClickListener(){
-           @Override
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-               if (filterOptionsLayer.getVisibility() == View.VISIBLE) {
-                   filterOptionsLayer.setVisibility(View.INVISIBLE);
-               } else {
-                   filterOptionsLayer.setVisibility(View.VISIBLE);
-               }
+                if (filterOptionsLayer.getVisibility() == View.VISIBLE) {
+                    filterOptionsLayer.setVisibility(View.INVISIBLE);
+                } else {
+                    filterOptionsLayer.setVisibility(View.VISIBLE);
+                }
             }
         });
 
@@ -190,8 +218,8 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
 
 
     /*
-    *  On press of back button exit the app
-    */
+     *  On press of back button exit the app
+     */
     @Override
     public void onBackPressed() {
         finishAffinity();
@@ -210,10 +238,102 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
 
-        // Add a marker in Sydney and move the camera
-        LatLng central_campus = new LatLng(32.880283, -117.237556);
-        mMap.addMarker(new MarkerOptions().position(central_campus).title("Geisel"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(central_campus,16.0f));
+    }
+
+    public boolean checkUserLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Request_User_Location_Code);
+            }
+            else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Request_User_Location_Code);
+            }
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case Request_User_Location_Code:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        if (googleApiClient == null) {
+                            buildGoogleApiClient();
+                        }
+                        mMap.setMyLocationEnabled(true);
+                    }
+                }
+                else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this).addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lastLocation = location;
+
+        if (currentUserLocationMarker != null) {
+            currentUserLocationMarker.remove();
+        }
+
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("user Current Location");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+        currentUserLocationMarker = mMap.addMarker(markerOptions);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(14));
+
+        if(googleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(1100);
+        locationRequest.setFastestInterval(1100);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
