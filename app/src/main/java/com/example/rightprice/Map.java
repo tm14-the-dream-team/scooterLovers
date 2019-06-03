ca99f09c55b59a36e3b69d68de2d0829f79eb917
jerry1oo <jeremiah7847@gmail.com>
2019-06-02 22:29:25 -0700

    TEXTBUBBLE.2

--- app/src/main/java/com/example/rightprice/Map.java
+++ app/src/main/java/com/example/rightprice/Map.java
@@ -11,10 +11,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -101,29 +98,7 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
         }
 
-        private View.OnTouchListener handleTouch = new View.OnTouchListener() {
 
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                int x = (int) event.getX();
-                int y = (int) event.getY();
-
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        Log.i("TAG", "touched down");
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        Log.i("TAG", "moving: (" + x + ", " + y + ")");
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        Log.i("TAG", "touched up");
-                        break;
-                }
-
-                return true;
-            }
-        };
 
         if(mMap != null){
             mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
@@ -135,11 +110,11 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
 
                 @Override
-                public ViewGroup getInfoContents(Marker marker) {
-                    ViewGroup v = (ViewGroup)getLayoutInflater().inflate(R.layout.info_window, null);
+                public View getInfoContents(Marker marker) {
+                    View v = getLayoutInflater().inflate(R.layout.info_window, null);
 
                     LinearLayout parent = (LinearLayout)findViewById(R.id.TB);
-                    DrawView drawView = new DrawView(this);
+                    //DrawView drawView = new DrawView(this);
 
 
                     TextView TBservice = (TextView) v.findViewById(R.id.TB_service);
