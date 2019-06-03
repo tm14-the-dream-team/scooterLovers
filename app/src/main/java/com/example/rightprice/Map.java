8cbea3eca03b50914ee6ca324d9dd92c859d1381
Kevin Lee <khl089@ucsd.edu>
2019-06-02 17:14:51 -0700

    Forgot Password Functional, Fixed UI

--- app/src/main/java/com/example/rightprice/Map.java
+++ app/src/main/java/com/example/rightprice/Map.java
@@ -3,6 +3,7 @@ package com.example.rightprice;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.PorterDuff;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
